0ff6bea81b4c1458700d4552f3218f5b111a452c
MacSupportPlus <71103657+MacSupportPlus@users.noreply.github.com>
2020-12-01 08:07:21 -0600

    components, components

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+class HelloWorld {
+    public String printHelloWorld() {
+        System.out.println("from printHelloWorld");
+        return "My name is Jason McElvain I am 100 years old My hometown is Chattanooga, TN";
+    }
+}
---
+++ HelloWorldTest.java
@@ -0,0 +1,7 @@
+public class HelloWorldTest {
+    public static void main (String[] args){
+        HelloWorld HW = new HelloWorld();
+        String result = HW.printHelloWorld();
+        System.out.println(result);
+    }
+}
